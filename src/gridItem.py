8077be8b2f442fc670bec6b8e4abfe838d349f38
Nhat <hoathinhvietnam2016@gmail.com>
2023-03-24 19:24:08 +0700

    add zoom and Pan, it still have bugs

--- src/gridItem.py
+++ src/gridItem.py
@@ -1,6 +1,6 @@
 import sys
 from PySide2.QtCore import Qt, QPointF
-from PySide2.QtGui import QPen, QColor, QPainterPath
+from PySide2.QtGui import QPen, QColor, QPainterPath, QPainter
 from PySide2.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsPathItem
 
 class GraphicsView(QGraphicsView):
@@ -19,6 +19,22 @@ class GraphicsView(QGraphicsView):
 
         self.setAlignment(Qt.AlignLeft | Qt.AlignTop)
         self.setBackgroundBrush(QColor(self.background_color))
+
+        self.setRenderHints(QPainter.Antialiasing | QPainter.SmoothPixmapTransform)
+        # Enable mouse tracking
+        self.setMouseTracking(True)
+        self.setInteractive(True)
+
+        # Enable pan and zoom
+        self.setRenderHint(QPainter.Antialiasing)
+        self.setRenderHint(QPainter.SmoothPixmapTransform)
+        self.setOptimizationFlag(QGraphicsView.DontAdjustForAntialiasing, True)
+        self.setOptimizationFlag(QGraphicsView.DontSavePainterState, True)
+        self.setViewportUpdateMode(QGraphicsView.FullViewportUpdate)
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+
+        self._pan = False
+        self._pan_start = QPointF()
 
     def create_grid_item(self, width, height, spacing):
         item = QGraphicsPathItem()
@@ -39,6 +55,42 @@ class GraphicsView(QGraphicsView):
         item.setPath(path)
 
         return item
+
+    def wheelEvent(self, event):
+        zoom_factor = 1.15
+        if event.angleDelta().y() < 0:
+            zoom_factor = 1 / zoom_factor
+
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        self.scale(zoom_factor, zoom_factor)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.RightButton:
+            self._pan = True
+            self._pan_start = event.pos()
+            self.setCursor(Qt.ClosedHandCursor)
+            event.accept()
+        else:
+            super().mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if self._pan:
+            delta = self.mapToScene(event.pos()) - self.mapToScene(self._pan_start)
+            self.setTransformationAnchor(QGraphicsView.NoAnchor)
+            self.translate(delta.x(), delta.y())
+            self._pan_start = event.pos
+            event.accept()
+        else:
+            super().mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.RightButton:
+            self._pan = False
+            self.setCursor(Qt.ArrowCursor)
+            event.accept()
+        else:
+            super().mouseReleaseEvent(event)
+
 if __name__ == '__main__':
     app = QApplication([])
     view = GraphicsView(background_color=QColor(0, 0, 0), grid_color=QColor(230, 230, 230))
